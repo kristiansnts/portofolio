f0ad4b67ba55fc323a74c278d269505f1ed57afe
kristiansnts <epafroditus.kristian@gmail.com>
2024-02-10 09:20:43 +0700

    fix images url

--- src/data/projects.js
+++ src/data/projects.js
@@ -2,21 +2,21 @@ export const projects = [
     {
         "title": "Quiz App",
         "tools": ["React JS", "API"],
-        "img": "../public/QuizProject.jpg",
+        "img": "/QuizProject.jpg",
         "link": "https://github.com/kristiansnts/dot-intern-challenge",
         "demo": "https://fastidious-shortbread-bc6521.netlify.app/"
     },
     {
         "title": "Age Calculator App",
         "tools": ["React JS"],
-        "img": "../public/ageCalculator2.jpg",
+        "img": "/ageCalculator2.jpg",
         "link": "https://github.com/kristiansnts/fem-age-calculator",
         "demo": "https://unrivaled-cuchufli-175eea.netlify.app/"
     },
     {
         "title": "Advice Generator App",
         "tools": ["React JS", "API"],
-        "img": "../public/adviceGenerator2.jpg",
+        "img": "/adviceGenerator2.jpg",
         "link": "https://github.com/kristiansnts/fem-api-advice-generator",
         "demo": "https://exquisite-zuccutto-ce61b0.netlify.app/"
     },
